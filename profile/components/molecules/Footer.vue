6bdeb7381e8892a9b1c9bc73cf1c5f34c5317260
@redimpulz.s.uechi <s.uechi@redimpulz.com>
2019-09-19 12:20:17 +0900

    footerに各種リンク(github等)を追加

--- profile/components/molecules/Footer.vue
+++ profile/components/molecules/Footer.vue
@@ -1,6 +1,30 @@
 <template>
   <footer class="footer is-primary">
-    <h3>u-sho</h3>
-    <p class="has-text-grey">Copyright © 2019 Shohei Uechi.</p>
+    <section class="content">
+      <h3 class="has-text-dark">u-sho.github.io</h3>
+      <p class="has-text-grey">Copyright © 2019 Shohei Uechi.</p>
+    </section>
+    <div class="block">
+      <a href="https://github.com/u-sho" exact-active-class="is-active">
+        <b-icon icon="github-circle" size="is-medium" type="is-dark" />
+      </a>
+      <a href="https://twitter.com/u_sho_KyoPro" exact-active-class="is-active">
+        <b-icon icon="twitter" size="is-medium" type="is-info" />
+      </a>
+      <a href="mailto:720187sho@gmail.com" exact-active-class="is-active">
+        <b-icon icon="gmail" size="is-medium" type="is-danger" />
+      </a>
+      <a href="https://u-sho.hatenablog.com/" exact-active-class="is-active">
+        <HatenaLogo />
+      </a>
+    </div>
   </footer>
 </template>
+
+<script>
+export default {
+  components: {
+    HatenaLogo: () => import('~/components/atoms/HatenablogLogo')
+  }
+}
+</script>
