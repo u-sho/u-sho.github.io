374befe6828e7d8b1689b9070542a09144951395
@redimpulz.s.uechi <s.uechi@redimpulz.com>
2019-09-19 10:51:40 +0900

    extracted header component

---
+++ profile/components/molecules/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <nav
+      class="navbar has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img src="~assets/buefy.png" alt="Buefy" height="28" />
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
